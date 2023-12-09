5e857d760af223027883795dc49fb1293e8ad7e3
DevT3e <45059485+DevT3e@users.noreply.github.com>
2023-12-09 10:44:52 +0300

    Add files via upload

---
+++ RPI(LAB7-9)/src/service/taskService.js
@@ -0,0 +1,12 @@
+import { tasks } from "../mock/task.js";
+
+export class TasksService {
+    #boardTasks = tasks;
+    getBoardTasks() {
+        return this.#boardTasks;
+    }
+    
+    getTasksByStatus(status) {
+    return this.#boardTasks.filter(task => task.status === status);
+  }
+}
